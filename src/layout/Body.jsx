84f637928861b1063b0957c05a9428a88468c9ea
esdncalingo <esdncalingo@gmail.com>
2022-11-20 19:50:17 +0800

    init

---
+++ src/layout/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div className="flex">
+        <div>Navigation</div>
+        <div>Messages</div>
+        <div>Chat</div>
+        <div>Members</div>
+    </div>
+  )
+}
---
+++ src/pages/Apicall.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect,useMemo } from 'react'
+
+export default function Apicall() {
+
+    const [accessData, setAccessData] = useState({})
+
+    const userData = {
+        email: 'michelle@email.com',
+        password: '12345678',
+    }
+
+    const userMessage = {
+        "receiver_id": 2919,
+        "receiver_class": "User",
+        "body": "hello world"
+    }
+
+    const fetchSlackAppApi = async () => {
+        const res = await fetch('http://206.189.91.54/api/v1/auth/sign_in', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', 
+            },
+            body: JSON.stringify(userData)
+        });
+
+        await setAccessData({
+            'access-token': res.headers.get('access-token'),
+            client: res.headers.get('client'),
+            expiry: res.headers.get('expiry'),
+            uid: res.headers.get('uid')
+        });
+        await console.log(accessData)  
+    }
+
+    const fetchSlackAppMesage = async () => {
+        const response = await fetch('http://206.189.91.54/api/v1/messages?receiver_id=2914&receiver_class=User', {
+            method: 'Get',
+            headers: {
+                'Content-Type': 'application/json', 
+                ...accessData
+            },
+            // body: JSON.stringify(userMessage)
+        });
+        const data = await response.json();
+    }
+    
+    useEffect(()=>{
+        fetchSlackAppApi();
+        // fetchSlackAppMesage();
+    },[])
+    
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+// http://206.189.91.54/api/v1/auth/sign_in?email=${email}&password=${password}&pasword_confirmation=${cpassword}
