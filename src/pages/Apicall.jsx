1dd7fbe8ecd3b086b666df4223f6fee5d66dc94d
esdncalingo <esdncalingo@gmail.com>
2022-11-20 22:57:56 +0800

    update apicall.jsx

--- src/pages/Apicall.jsx
+++ src/pages/Apicall.jsx
@@ -65,7 +65,7 @@ export default function Apicall() {
     useEffect(()=>{
         fetchSlackAppApi();
         // fetchSlackAppMesage();
-        fetchAddMember();
+        // fetchAddMember();
     },[])
     
 
