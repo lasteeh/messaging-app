9343c9235c41515a9d44c60e9f6e3b15850a19e3
lasteeh <lastdavid.lantican@gmail.com>
2022-12-03 01:37:30 +0800

    style:fix chat messages style

--- src/components/ChatItem.jsx
+++ src/components/ChatItem.jsx
@@ -6,38 +6,47 @@ import { calculateTime } from "../helper/functions";
 
 const ChatItem = (props) => {
   const { accessData } = useContext(ApiContext);
+  const isFromSender = accessData.id === props.sender.id;
 
   return (
     <div
-      className={`flex flex-row flex-nowrap items-end gap-[0.8rem] w-[100%] h-[max-content] pl-[1rem] pr-[1rem] animate-swipeUp ${
-        accessData.id === props.sender.id && "flex-row-reverse"
-      }`}
+      className={`flex ${
+        isFromSender ? "flex-row-reverse" : "flex-row"
+      } flex-nowrap  items-start  gap-[0.5rem] animate-swipeUp`}
     >
-      <div
-        className={
-          !props.pictoggle
-            ? "invisible photo-holder aspect-square w-[min(28px,_100%)]"
-            : "photo-holder aspect-square w-[min(28px,_100%)] text-white  p-[0.4rem] mb-auto rounded-[0.35rem] grid place-items-center"
-        }
-      >
-        <FontAwesomeIcon icon={faUser} className="w-[100%] h-[100%]" />
+      <div className="w-[28px] shrink-0 ">
+        <div
+          className={`photo-holder w-[100%]  text-white p-[0.4rem] mb-auto rounded-[0.35rem] grid place-items-center ${
+            !props.pictoggle ? "invisible" : ""
+          }`}
+        >
+          <FontAwesomeIcon icon={faUser} className="w-[100%] h-[100%]" />
+        </div>
       </div>
-      <div className="flex flex-col flex-nowrap gap-[0.1rem] ">
+
+      <div
+        className={`max-w-[1000px] ${isFromSender ? "pl-[36px]" : "pr-[36px]"}`}
+      >
         {props.toggle && (
-          <div className={`${accessData.id === props.sender.id && "ml-auto"}`}>
+          <div
+            className={`flex ${
+              isFromSender ? "ml-auto flex-row" : "flex-row"
+            } mb-[4px]`}
+          >
             <span className="sender text-[0.6rem] pl-[6px]">
               {props.sender.uid}
             </span>
             <span className="sender text-[0.6rem] pl-[6px]">
-              {`(${props.sender.id})`}
+              {/* {`(${props.sender.id})`} */}
             </span>
             <span className="time-holder lowercase text-[0.6rem] pl-[6px]">
               {calculateTime(props.time)}
             </span>
           </div>
         )}
+
         <div className="messages-holder relative w-[100%]">
-          <p className={`${accessData.id === props.sender.id && "ml-auto"}`}>
+          <p className={`${isFromSender && "ml-auto"} max-w-[900px]`}>
             {props.body}
           </p>
         </div>
