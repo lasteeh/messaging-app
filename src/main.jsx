64aa7dd226be79a804c0ba87b9e2e24ab1f7a539
Last David Lantican <lastdavid.lantican@gmail.com>
2023-06-12 11:14:54 +0800

    config to deploy on github pages on same repo

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/messaging-app/dist/">
     <App />
   </BrowserRouter>
 );
